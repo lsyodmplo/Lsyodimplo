8bad15e4f013dc405ac7bb06a238837cb5076901
lsyodmplo <accdamelq500k@gmail.com>
2025-12-29 20:02:21 +0700

    Add files via upload

--- keys.js
+++ keys.js
@@ -6,11 +6,13 @@ window.VIP_VALID_KEYS = [
     "VIP-2024-DEMO-TEST-0001",
     "VIP-ABCD-EFGH-IJKL-MNOP",
     "VIP-1234-5678-9ABC-DEF0",
+    "VIP-TDZO-6LTN-GXI8-UR50",
+    "VIP-313N-DKX5-YGVJ-ZCXU",
     "VIP-LNAI-F2MC-L9F2-RLZC",
     "VIP-T6F9-GQO1-RL3U-7OTR",
     "VIP-6DY4-US52-BG19-EM4O",
     "VIP-638D-D213-2E8F-3CAD"
 ];
 
-// Last updated: 29/12/2025 19:38:45
-// Total keys: 7 (3 demo + 4 generated)
+// Last updated: 29/12/2025 19:53:27
+// Total keys: 9 (3 demo + 6 generated)
